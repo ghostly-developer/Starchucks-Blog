422c0e98210e67a126a28af5033b0271c6553463
RanggaGunarto <ranggagunarto@gmail.com>
2021-08-17 00:43:32 +0800

    Change All URL

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
             <h1>Starchucks Weekly</h1>
             <div className="links">
                 <Link to="/Starchucks-Blog">Home</Link>
-                <Link to="/create">New Blog</Link>
+                <Link to="/Starchucks-Blog/create">New Blog</Link>
             </div>
         </nav>
     );
